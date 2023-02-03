9cd7d696f4d804605372b638446f23b340a9a28c
vinix <viniwell2807@gmail.com>
2023-02-03 22:51:53 +0000

    finished catcher

--- catcher.py
+++ catcher.py
@@ -40,6 +40,15 @@ class Catcher:
         self.rect.x=self.x
     def blitme(self):
         self.screen.blit(self.image, self.rect)
+class Gameover:
+    def __init__(self, catcher_game):
+        self.screen=catcher_game.screen
+        self.screen_rect=self.screen.get_rect()
+        self.image=pygame.image.load('gameover.png')
+        self.rect=self.image.get_rect()
+        self.rect.center=self.screen_rect.center
+    def blitme(self):
+        self.screen.blit(self.image, self.rect)
 class Game:
     def __init__(self):
         self.screen = pygame.display.set_mode((1200, 800))
@@ -48,11 +57,17 @@ class Game:
         self.catcher=Catcher(catcher_game=self)
         self.ball=Ball(x=random.randint(0, self.screen_width), y=random.randint(0, self.screen_height-200),catcher_game=self)
         self.catcher.rect.bottom=self.screen.get_rect().bottom
+        self.loses=0
+        self.game=True
+        self.game_over=Gameover(self)
     def run_game(self):
         while True:
-            self._check_ball()
+            if self.game:
+                self._check_loses()
+                self._check_ball()
+                self._check_events()
+                self._update_catcher()
             self._check_events()
-            self._update_catcher()
             self._update_screen()
     def _update_catcher(self):
         self.catcher.update()
@@ -71,14 +86,23 @@ class Game:
                 if event.key==pygame.K_LEFT:
                     self.catcher.moving_left=False
     def _check_ball(self):
-        if (self.catcher.rect.top<=self.ball.rect.bottom<self.catcher.rect.bottom and (self.catcher.rect.left<self.ball.rect.left<self.catcher.rect.right or self.catcher.rect.left<self.ball.rect.right<self.catcher.rect.right)) or self.ball.rect.bottom==self.screen_height:
+        if (self.catcher.rect.top<=self.ball.rect.bottom<self.catcher.rect.bottom and (self.catcher.rect.left<self.ball.rect.left<self.catcher.rect.right or self.catcher.rect.left<self.ball.rect.right<self.catcher.rect.right)):
             self.ball=Ball(x=random.randint(0, self.screen_width), y=random.randint(0, self.screen_height-200),catcher_game=self)
+        elif self.ball.rect.bottom==self.screen_height:
+            self.ball=Ball(x=random.randint(0, self.screen_width), y=random.randint(0, self.screen_height-200),catcher_game=self)
+            self.loses+=1
         else:
             self.ball.update()
+    def _check_loses(self):
+        if self.loses>=3:
+            self.game=False
     def _update_screen(self):
         self.screen.fill((230, 230, 230))
-        self.catcher.blitme()
-        self.ball.blitme()
+        if self.game:
+            self.catcher.blitme()
+            self.ball.blitme()
+        else:
+            self.game_over.blitme()
         pygame.display.flip()
 game=Game()
 game.run_game()
